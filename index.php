0cdf0a3f27ff9e424337dfce34d416f7e03a94fd
Steven Childrey <steven.childrey36@yahoo.com>
2017-04-19 22:23:25 -0400

    Initail commit

---
+++ index.php
@@ -0,0 +1,8 @@
+<html>
+<head>
+    <title>Home | Watch the Style</title>
+    <link rel='stylesheet' type='text/css' href='main.css'>
+</head>
+<body>
+    <h1 class='title'>Watch the Style</h1>
+</body>
