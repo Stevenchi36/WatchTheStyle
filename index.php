6929220515984839144672cb5ed7b758bf89fe08
Steven Childrey <steven.childrey36@yahoo.com>
2017-04-20 15:03:56 -0400

    Nav bar

--- index.php
+++ index.php
@@ -1,8 +1,28 @@
+<!doctype html>
 <html>
 <head>
-    <title>Home | Watch the Style</title>
-    <link rel='stylesheet' type='text/css' href='main.css'>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+
+  <title>Home | Watch the Style</title>
+
+  <link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:400,200,300,600,700,900' rel='stylesheet' type='text/css'>
+  <link rel="icon" href="/assets/favicon.png" type="image/x-icon">
+  <link rel='stylesheet prefetch' href='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css'>
+<!--  <link rel="stylesheet" type="text/css" href="style.css">-->
+  <link rel="stylesheet" type="text/css" href="master.css">
+  <link rel="stylesheet" type="text/css" href="homePage.css">
 </head>
 <body>
-    <h1 class='centerText'>Watch the Style</h1>
+    <nav class="hidden-xs navbar-fixed">
+        <ul>
+            <li id="brandItem" class="brandItem"><p class="brand">Watch the Style</p></li>
+            <li><a href="" class="active aboutMe" onClick="changeActiveAbout()" title="Go to About Me">New</a></li>
+            <li><a href="" class="projects" onClick="changeActiveProj()" title="Go to Projects">Search</a></li>
+            <li style="float:right" class="contactMeList"><a class="contactMe" href="/register" onClick="changeActiveContact()" title="Go to Contact Info">Register</a></li>
+            <li style="float:right" class="contactMeList"><a class="contactMe" href="/login" onClick="changeActiveContact()" title="Go to Contact Info">Login</a></li>
+        </ul>
+    </nav>
 </body>
+</html>
