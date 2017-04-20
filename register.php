258d042438e3b5201090db671365a11e6bc328c4
Steven Childrey <steven.childrey36@yahoo.com>
2017-04-20 01:48:33 -0400

    Add register page

---
+++ register.php
@@ -0,0 +1,8 @@
+<html>
+<head>
+    <title>Register | Watch the Style</title>
+    <link rel='stylesheet' type='text/css' href='main.css'>
+</head>
+<body>
+    <h1 class='center'>Register</h1>
+</body>
