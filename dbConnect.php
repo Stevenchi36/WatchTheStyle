cec056e615c6185059f2a022372fc5e1cfd1bfde
Steven Childrey <steven.childrey36@yahoo.com>
2017-04-21 02:32:19 -0400

    Add connection for MySQL

---
+++ dbConnect.php
@@ -0,0 +1,26 @@
+<?php
+    function db_connect() {
+
+        static $connection;
+
+        if(!isset($connection)) {
+
+            $config = parse_ini_file('/var/www/private/config.ini');
+            $connection = mysqli_connect($config['servername'], $config['username'], $config['password'], $config['dbname']);
+
+        }
+
+        if(!$connection) {
+
+            return mysqli_connect_error();
+
+        }
+        return $connection;
+    }
+
+    $connection = db_connect();
+
+    if ($connection->connect_error) {
+        die("Connection failed: " . $connection->connect_error);
+    }
+?>
