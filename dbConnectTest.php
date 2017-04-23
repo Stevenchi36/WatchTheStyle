eba4e11fa6dc10cbe5f9de7ae9c3942a3fc6ae43
Steven Childrey <steven.childrey36@yahoo.com>
2017-04-23 13:33:50 -0400

    Retry

--- dbConnectTest.php
+++ dbConnectTest.php
@@ -8,7 +8,7 @@ $config = parse_ini_file('/var/www/private/config.ini');
 //$dbpass = 'pass';
 //$dbhost = 'host';
 $connect = mysql_connect($config['servername'], $config['username'], $config['password']) or die("Unable to Connect to '$dbhost'");
-mysql_select_db($config['dbname']) or die("Could not open the db '$dbname'");
+mysql_select_db($config['dbname']) or die("Could not open the db '$config['dbname']'");
 $test_query = "SHOW TABLES FROM $dbname";
 $result = mysql_query($test_query);
 $tblCnt = 0;
