ce1d56cf59e9ca526cbd41b6110bd4b52be98b53
Steven Childrey <steven.childrey36@yahoo.com>
2017-04-24 19:29:55 -0400

    Add script for PW

--- register.php
+++ register.php
@@ -16,6 +16,7 @@
   <link rel="icon" href="Assets/favicon.png" type="image/x-icon">
   <link rel='stylesheet prefetch' href='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css'>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="javascript/register.js"></script>
   <link rel="stylesheet" type="text/css" href="master.css">
   <link rel="stylesheet" type="text/css" href="register.css">
 </head>
