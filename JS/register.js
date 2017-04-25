e0b9fa484fb28d0cf877ff2bcb559271d6ad5d71
Steven Childrey <steven.childrey36@yahoo.com>
2017-04-24 20:40:08 -0400

    Change error messages

--- JS/register.js
+++ JS/register.js
@@ -2,11 +2,11 @@ $(document).ready(function(){
     $('#password1, #password2').on('keyup', function(){
 
         if($('#password1').val() == $('#password2').val()){
-            $('#pwError').html('Matching').css('color', 'green');
+            $('#pwError').html('Passwords Match').css('color', 'green');
             $('#register').removeAttr('disabled');
         }
         else{
-            $('#pwError').html('Not Matching').css('color','red');
+            $('#pwError').html("Passwords Don't Match").css('color','red');
             $('#register').attr('disabled','disabled');
         }
 
