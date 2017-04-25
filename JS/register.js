da92dd4d188b760989fe001f4db51818c65beb7e
Steven Childrey <steven.childrey36@yahoo.com>
2017-04-25 01:29:31 -0400

    removed ;

--- JS/register.js
+++ JS/register.js
@@ -10,7 +10,7 @@ $(document).ready(function(){
 			if($('#password1').val() == $('#password2').val()){
 				$('#pwError').html('Passwords Match').css('color', 'green');
 				$('#register').removeAttr('disabled');
-				$('.btnRegister').css('background-color','#6495ed;');
+				$('.btnRegister').css('background-color','#6495ed');
 			}
 			else{
 				$('#pwError').html("Passwords Don't Match").css('color','red');
