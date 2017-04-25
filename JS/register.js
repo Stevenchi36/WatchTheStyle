fe5fdd725b5fa8908013a915d74b3b2aa43343d8
Steven Childrey <steven.childrey36@yahoo.com>
2017-04-25 01:13:45 -0400

    Added length validation

--- JS/register.js
+++ JS/register.js
@@ -1,14 +1,18 @@
 $(document).ready(function(){
     $('#password1, #password2').on('keyup', function(){
 
-        if($('#password1').val() == $('#password2').val()){
-            $('#pwError').html('Passwords Match').css('color', 'green');
-            $('#register').removeAttr('disabled');
-        }
-        else{
-            $('#pwError').html("Passwords Don't Match").css('color','red');
-            $('#register').attr('disabled','disabled');
-        }
-
+		if($('#password1').val().length < 8){
+			$('#pwError').html('Password must be at least 8 characters.').css('color','red');
+		}
+		else{
+			if($('#password1').val() == $('#password2').val()){
+				$('#pwError').html('Passwords Match').css('color', 'green');
+				$('#register').removeAttr('disabled');
+			}
+			else{
+				$('#pwError').html("Passwords Don't Match").css('color','red');
+				$('#register').attr('disabled','disabled');
+			}
+		}
     });
 });
