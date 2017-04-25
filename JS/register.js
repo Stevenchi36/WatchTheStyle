68e1e19053c4a21f05fac1fd5e19f70b1fc56d15
Steven Childrey <steven.childrey36@yahoo.com>
2017-04-24 20:26:58 -0400

    Retry

--- JS/register.js
+++ JS/register.js
@@ -1,12 +1,14 @@
-$('#password1', '#password2').on('keyup', function(){
+$(document).ready(function(){
+    $('#password1, #password2').on('keyup', function(){
 
-    if($('#password1').val() == $('#password2').val()){
-        $('#pwError').html('Matching').css('color', 'green');
-        $('#register').removeAttr('disabled');
-    }
-    else{
-        $('#pwError').html('Not Matching').css('color','red');
-        $('#register').attr('disabled','disabled');
-    }
+        if($('#password1').val() == $('#password2').val()){
+            $('#pwError').html('Matching').css('color', 'green');
+            $('#register').removeAttr('disabled');
+        }
+        else{
+            $('#pwError').html('Not Matching').css('color','red');
+            $('#register').attr('disabled','disabled');
+        }
 
+    });
 });
