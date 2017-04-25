1bc99fb90cee1828c072a553e6e0d6e8ff0f9363
Steven Childrey <steven.childrey36@yahoo.com>
2017-04-25 01:28:14 -0400

    color change button

--- JS/register.js
+++ JS/register.js
@@ -3,15 +3,19 @@ $(document).ready(function(){
 
 		if($('#password1').val().length < 8){
 			$('#pwError').html('Password must be at least 8 characters.').css('color','red');
+			$('#register').attr('disabled','disabled');
+			$('.btnRegister').css('background-color','grey');
 		}
 		else{
 			if($('#password1').val() == $('#password2').val()){
 				$('#pwError').html('Passwords Match').css('color', 'green');
 				$('#register').removeAttr('disabled');
+				$('.btnRegister').css('background-color','#6495ed;');
 			}
 			else{
 				$('#pwError').html("Passwords Don't Match").css('color','red');
 				$('#register').attr('disabled','disabled');
+				$('.btnRegister').css('background-color','grey');
 			}
 		}
     });
