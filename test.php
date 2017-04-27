9bbd70e4759fdd0cf271baa48a9645b46f946003
Steven Childrey <steven.childrey36@yahoo.com>
2017-04-27 00:24:17 -0400

    test

--- test.php
+++ test.php
@@ -1,9 +1,9 @@
 <?php
 
+	$connection = mysqli_connect($config['servername'], $config['username'], $config['password'], $config['dbname']);
 	session_start();
 //    require_once('dbConnect.php');
 	$config = parse_ini_file('/var/www/private/config.ini');
-	$connection = mysqli_connect($config['servername'], $config['username'], $config['password'], $config['dbname']);
 	$query = "INSERT INTO users(userName, userEmail,userPass) VALUES(hello,hello,hello);";
 	mysqli_query($connection, $query);
 	header("location: index.php");
