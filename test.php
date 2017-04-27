0d5161b028933fd8d9aca257385df10b704daede
Steven Childrey <steven.childrey36@yahoo.com>
2017-04-27 00:45:31 -0400

    retry

--- test.php
+++ test.php
@@ -1,11 +1,19 @@
 <?php
 
-	$connection = mysqli_connect("localhost", $config['username'], $config['password'], $config['dbname']);
-	session_start();
-//    require_once('dbConnect.php');
 	$config = parse_ini_file('/var/www/private/config.ini');
-	$query = "INSERT INTO users(userName, userEmail,userPass) VALUES(hello,hello,hello);";
-	mysqli_query($connection, $query);
-	header("location: index.php");
+	$connection = mysqli_connect("localhost", $config['username'], $config['password'], $config['dbname']);
+	if(!$connection){
+		die("Connection failed: " . mysqli_connect_error());
+	}
+	$query = "INSERT INTO users(userName, userEmail,userPass) VALUES('hello','hello','hello')";
+
+	if(mysqli_query($connection, $query)){
+		echo "New record created successfully";
+	}
+	else{
+		echo "Error: " . $query . "<br>" . mysqli_error($connection);
+	}
+
+	mysqli_close($connection);
 
 ?>
