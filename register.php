28011b9a4fd7424b3d04e60b6214487ed432876c
Steven Childrey <steven.childrey36@yahoo.com>
2017-04-29 01:21:28 -0400

    simple comment

--- register.php
+++ register.php
@@ -1,5 +1,6 @@
 <?php
 
+	//Connect
 	$config = parse_ini_file('/var/www/private/config.ini');
 	$connection = mysqli_connect("localhost", $config['username'], $config['password'], $config['dbname']);
 	if(!$connection){
