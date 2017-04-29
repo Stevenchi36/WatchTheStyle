59b6559b3e0ebc67078fcc97bd0da18dbce25620
Steven Childrey <steven.childrey36@yahoo.com>
2017-04-29 16:38:32 -0400

    Add form

--- login.php
+++ login.php
@@ -11,7 +11,8 @@
   <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed" rel="stylesheet">
   <link rel="icon" href="Assets/favicon.png" type="image/x-icon">
   <link rel='stylesheet prefetch' href='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css'>
-  <link rel="stylesheet" type="text/css" href="master.css">
+  <link rel="stylesheet" type="text/css" href="CSS/master.css">
+  <link rel="stylesheet" type="text/css" href="CSS/login.css">
 </head>
 <body>
     <nav class="hidden-xs navbar-fixed">
@@ -23,5 +24,18 @@
             <li style="float:right" class="contactMeList"><a class="contactMe active" href="/login.php" title="Login to account">Login</a></li>
         </ul>
     </nav>
+    <form method='post' action='login.php'>
+    	<table class="loginOptions">
+    		<tr>
+    			<td><input type='text' name="usernameInput" id='usernameInput' pattern="^[a-zA-Z0-9_-]{4,20}$" maxlength="20" size='50' placeholder="Username"  required /></td>
+    		</tr>
+    		<tr>
+    			<td><input type='password' name='passwordInput' id='password' pattern=".{8,}" maxlength="256" size='50' placeholder="Password" required/></td>
+    		</tr>
+    		<tr>
+    			<td><input type='submit' class='btnLogin' value='Login' id='login' name='btnLogin' disabled/></td>
+    		</tr>
+    	</table>
+    </form>
 </body>
 </html>
