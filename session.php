e0ad31272d7ab422aea33acdd0a24a69396ba7ce
Steven Childrey <steven.childrey36@yahoo.com>
2017-05-02 23:14:56 -0400

    retry include connection

--- session.php
+++ session.php
@@ -1,10 +1,12 @@
 <?php
 
-	$config = parse_ini_file('/var/www/private/config.ini');
-	$connection = mysqli_connect("localhost", $config['username'], $config['password'], $config['dbname']);
-	if(!$connection){
-		die("Connection failed: " . mysqli_connect_error());
-	}
+//	$config = parse_ini_file('/var/www/private/config.ini');
+//	$connection = mysqli_connect("localhost", $config['username'], $config['password'], $config['dbname']);
+//	if(!$connection){
+//		die("Connection failed: " . mysqli_connect_error());
+//	}
+
+	include ('dbConnect');
 
 
 	session_start();
